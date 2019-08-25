c393ad1d2be121b1099497fe4f70209c6623cd7d
Matt Kenney <mkenney@mjk-tech.com>
2019-08-24 20:17:07 -0400

    reorg server code and add comments

--- server/index.js
+++ server/index.js
@@ -8,26 +8,29 @@ const path = require('path');
 const cors = require('cors');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-
-const { typeDefs, resolvers } = require('../dist/schema');
 const ssr = require('../dist/ssr').default;
+
+// set up GraphQL server
+const { typeDefs, resolvers } = require('../dist/schema');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+// read in the empty HTML template, and split it where
+// we will insert server-side rendered HTML
 const build = path.join(path.dirname(__dirname), 'build');
-const template = fs
-        .readFileSync(path.join(build, 'index.html'), 'utf8')
-        .replace(/%([^%]*)%/g, (m, key) => process.env[key] || '')
-        ;
 const root = '<div id="root">';
-const [ prelude, coda ] = template.split(root, 2);
+const [ prelude, coda ] = fs
+            .readFileSync(path.join(build, 'index.html'), 'utf8')
+            .split(root, 2);
 
+// set up Express
 const app = new express();
 app.disable('x-powered-by');
 
+// in dev mode, allow cors from local webpack-dev-server
 if (app.settings.env === 'development')
 {
     app.use(cors({ origin: 'http://localhost:3000' }));
@@ -39,10 +42,13 @@ app.get('/index.html', function (req, res) {
   res.status(302).end();
 });
 
-server.applyMiddleware({ app });
+// serve /static/* and other static files
+app.use(express.static(build, { index: false, redirect: false }));
 
-app.use(express.static(build, { index: false }));
+// serve /graphql
+server.applyMiddleware({ app, cors: false });
 
+// server-side render React app
 app.use(function (req, res) {
   ssr().then(content => {
     const html = [ prelude, root, content, coda ].join('');
@@ -50,6 +56,7 @@ app.use(function (req, res) {
   });
 });
 
+// ready to serve
 app.listen({ port: 4000 }, () =>
   console.log('Server ready at http://localhost:4000')
 );
