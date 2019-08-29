9c72dce1154e6b98b9a327cf33f617dcac0a9c36
Matt Kenney <mkenney@mjk-tech.com>
2019-08-29 07:20:49 -0400

    add React Router Link for testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 const query = gql`query{hello}`;
 
@@ -23,9 +23,15 @@ function Home() {
 export default function App(props)
 {
   return (
-    <Switch>
+    <div>
+      <div>
+        <div><Link to="/">Home</Link></div>
+        <div><Link to="/about">About</Link></div>
+      </div>
+      <Switch>
         <Route path="/about" component={About}/>
         <Route component={Home}/>
-    </Switch>
+      </Switch>
+    </div>
   );
 }
